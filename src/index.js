9cf8e55727df8c10388de1f9b69a4574256144ec
mmclau14 <michael.j.mclaughlin20@gmail.com>
2019-02-09 15:39:45 -0600

    updated build scripts

--- src/index.js
+++ src/index.js
@@ -1,23 +1,24 @@
 // import 'react-dates/initialize'
+import React from 'react'
+import ReactDOM from 'react-dom'
 import '@babel/polyfill'
 import {
   App,
   LanguageProvider,
 } from 'js/component'
-import React from 'react'
-import ReactDOM from 'react-dom'
 import { Provider, } from 'unstated'
 import { Router, } from 'react-router-dom'
 import history from 'js/history'
 import 'scss/index.scss'
 // import socket from 'js/socket'
+import * as OfflinePluginRuntime from 'offline-plugin/runtime'
 import {
   translationMessages,
 } from './i18n'
 // Load the favicon and the .htaccess file
 import '!file-loader?name=[name].[ext]!./images/favicon.ico'
 import 'file-loader?name=.htaccess!./.htaccess' // eslint-disable-line import/extensions
-
+OfflinePluginRuntime.install()
 
 const MOUNT_NODE = document.getElementById('app')
 
@@ -40,8 +41,8 @@ if (!window.Intl) {
     resolve(import('intl'))
   })
     .then(() => Promise.all([
-        import('intl/locale-data/jsonp/en.js'),
-        import('intl/locale-data/jsonp/de.js')
+      import('intl/locale-data/jsonp/en.js'),
+      import('intl/locale-data/jsonp/de.js')
     ])) // eslint-disable-line prettier/prettier
     .then(() => render(translationMessages))
     .catch((err) => {
